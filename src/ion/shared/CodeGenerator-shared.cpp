98a47b731aa0916b9dac4a6a3e85af50f8a4b9f6
Zhilu Tang <zhilu.tang@gmail.com>
2013-03-05 15:12:25 +0000

    ignore this comment patch

--- src/ion/shared/CodeGenerator-shared.cpp
+++ src/ion/shared/CodeGenerator-shared.cpp
@@ -46,6 +46,7 @@ CodeGeneratorShared::generateOutOfLineCode()
     for (size_t i = 0; i < outOfLineCode_.length(); i++) {
         if (!gen->temp().ensureBallast())
             return false;
+//cause failure
         masm.setFramePushed(outOfLineCode_[i]->framePushed());
         lastPC_ = outOfLineCode_[i]->pc();
         sps_.setPushed(outOfLineCode_[i]->script());
